6d5d318572cbc1c59805249ed8a51270fc6607be
David Puerta Martín <88980449+david96182@users.noreply.github.com>
2024-04-30 01:34:22 -0400

    Fix vite.config.js error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://david96182.github.io/support-my-work/'
-  plugins: [react()],
+  base: 'https://david96182.github.io/support-my-work/',
+  plugins: [react()]
 })
